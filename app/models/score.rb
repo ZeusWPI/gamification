2490c7ded53de7cbd23e8c4bf8d5116e3e70d76b
Stijn Seghers <seghers_stijn@hotmail.com>
2014-06-24 23:12:52 +0200

    Added avatars

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,29 +1,27 @@
 class Score
-  attr_accessor :person, :points
+  attr_accessor :person, :points, :avatar
 
-  def initialize(person, points)
+  def initialize(person, points, avatar)
     @person = person
     @points = points
+    @avatar = avatar
   end
 
   def self.all
     github = Github.new
-    contributions = Hash.new 0
+    contributions = Hash.new
 
     # Fill hash with amount of commits per contributor for every repo
     github.repos.list(org: 'ZeusWPI').each do |repo|
       github.repos.contributors('ZeusWPI', repo.name).each do |cont|
-        contributions[cont.login] += cont.contributions
+        if contributions.has_key?(cont.login)
+          contributions[cont.login].points += cont.contributions
+        else
+          contributions[cont.login] = Score.new(cont.login, cont.contributions, cont.avatar_url)
+        end
       end
     end
 
-    # Convert hash to list of Score objects
-    scores = Array.new
-    contributions.each do |person, commits|
-      scores.push(Score.new(person, commits))
-    end
-
-    scores.sort_by! { |score| - score.points }
-    return scores
+    return contributions.values.sort_by { |score| - score.points }
   end
 end
