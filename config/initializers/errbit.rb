21a1a89858a2ee81aac0d9ad27cd4dcb9ba28c97
Tom Naessens <Tom.Naessens@UGent.be>
2015-03-17 08:45:25 +0100

    Move errbit port to 80

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,6 +1,6 @@
 Airbrake.configure do |config|
   config.api_key = '347b322e04831db3e24da517849b2a2b'
   config.host    = 'errbit-zeus.ilion.me'
-  config.port    = 443
+  config.port    = 80
   config.secure  = config.port == 443
 end
