5e04fd76bc45e8447336e9fd1fe77da180af5484
Ilion Beyst <ilion.beyst@gmail.com>
2015-03-20 04:38:56 +0100

    Use correct sort order

--- app/controllers/top4_controller.rb
+++ app/controllers/top4_controller.rb
@@ -2,7 +2,7 @@ class Top4Controller < ApplicationController
   include Schwarm
   def show
     @coders = Coder.with_stats(:score)
-      .where(date: 1.week.ago..Time.current).order(:score).take(4)
+      .where(date: 1.week.ago..Time.current).order(score: :desc).take(4)
 
     @repositories = Datenfisch.query.select(Coder.score)
       .where(date: 1.week.ago..Time.current).model(Repository)
