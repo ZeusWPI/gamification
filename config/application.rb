902b069ca2d2576ab1b2e4fc7559c6d7c959ceb4
Stijn Seghers <seghers_stijn@hotmail.com>
2015-03-20 02:25:31 +0100

    Fixed Ilion's "repairing views"

---
+++ config/application.rb
@@ -0,0 +1,52 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Gamification
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    config.time_zone = 'Brussels'
+    # autoload lib files
+    config.autoload_paths << Rails.root.join('lib')
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
+
+    # website url (required for setting webhooks)
+    config.url = ''
+
+    # organisations to track
+    config.organisations = [
+      # 'name'
+    ]
+    # individual repositories to track
+
+    config.repositories = [
+      # { name: 'name', org: 'organisation' }
+    ]
+
+    # Total bounty value
+    config.total_bounty_value = 5000
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
+  end
+end
