40566af8d3d4de5e779c4a0a1827e262395e389d
Ilion Beyst <ilion.beyst@gmail.com>
2015-03-28 15:37:23 +0100

    cleaned up webhooks_controller

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -3,57 +3,70 @@ class WebhooksController < ApplicationController
 
   def receive
     event = request.headers['X-Github-Event']
-    if Hooker.methods(false).include?(event.to_sym) # whitelist methods
-      payload = request.request_parameters['payload']
-      return head :bad_request unless payload
-      json = JSON.parse payload
-      Hooker.send(event, json)
-      head :created
-    else
-      head :ok
+    json = JSON.parse request.request_parameters['payload']
+    return head :bad_request unless event and json
+
+    case event
+      when 'push'
+        push json
+      when 'issues'
+        issues json
+      when 'repository'
+        repository json
+      else
+        head :ok
     end
   end
 
-  class Hooker
-    def self.push json
-      org = Organisation.find_by name: json['repository']['owner']['name']
-      repo = Repository.find_by name: json['repository']['name'],
-                                organisation: org
-      repo.pull
+  private
+  def push json
+    org = Organisation.find_by name: json['repository']['owner']['name']
+    repo = Repository.find_by name: json['repository']['name'],
+                              organisation: org
+    return head :ok unless org and repo
+    repo.pull
 
-      json['commits'].each do |commit|
-        Commit.register_from_sha repo, commit['id']
-      end
+    json['commits'].each do |commit|
+      Commit.register_from_sha repo, commit['id']
     end
+    head :created
+  end
 
-    def self.issues json
-      # get issue
-      org = Organisation.find_by name: json['repository']['owner']['login']
-      repo = Repository.find_by name: json['repository']['name'],
-                                organisation: org
-      issue = Issue.find_or_create_from_hash json['issue'], repo
+  def issues json
+    # get issue
+    org = Organisation.find_by name: json['repository']['owner']['login']
+    repo = Repository.find_by name: json['repository']['name'],
+                              organisation: org
+    return head :ok unless org and repo
 
-      case json['action']
-        when 'opened', 'reopened'
-          issue.update! closed_at: nil
-        when 'closed'
-          issue.close time: DateTime.parse(json['issue']['closed_at'])
-        when 'assigned'
-          assignee = Coder.find_by github_name: json['issue']['assignee']['login']
-          issue.update! assignee: assignee
-        when 'unassigned'
-          issue.update! assignee: nil
-      end
+    issue = Issue.find_or_create_from_hash json['issue'], repo
+
+    case json['action']
+      when 'opened', 'reopened'
+        issue.update! closed_at: nil
+      when 'closed'
+        issue.close time: DateTime.parse(json['issue']['closed_at'])
+      when 'assigned'
+        assignee = Coder.find_by github_name: json['issue']['assignee']['login']
+        issue.update! assignee: assignee
+      when 'unassigned'
+        issue.update! assignee: nil
+      else
+        return head :ok
     end
+    head :created
+  end
 
-    def self.repository json
-      org = Organisation.find_by name: json['repository']['owner']['login']
-      return unless org
+  def repository json
+    org = Organisation.find_by name: json['repository']['owner']['login']
+    return head :ok unless org
 
-      case json['action']
+    case json['action']
       when 'created'
         Repository.create name: json['repository']['name'], organisation: org
-      end
+      else
+        return head :ok
     end
+    head :created
   end
 end
