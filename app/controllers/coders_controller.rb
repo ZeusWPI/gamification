5cdd27f30ac2018ceadf4a08b27a36122b44544f
Ilion Beyst <ilion.beyst@gmail.com>
2015-04-10 23:54:11 +0200

    Added repositories to coder view

--- app/controllers/coders_controller.rb
+++ app/controllers/coders_controller.rb
@@ -2,6 +2,14 @@ class CodersController < ApplicationController
   before_action :set_coder, only: [:show]
 
   def show
+    @repositories = Datenfisch.query
+      .select(Coder.score,
+              Coder.commit_count,
+              Coder.additions,
+              Coder.deletions)
+      .model(Repository, inner_join: true)
+      .where(coder_id: @coder)
+      .order(score: :desc).run
   end
 
   private
