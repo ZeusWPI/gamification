77a3866e712a429eff85f1c2eb51bfb596982fa8
Ilion Beyst <ilion.beyst@gmail.com>
2015-04-12 20:18:31 +0200

    Avatars are links now.

--- app/helpers/coders_helper.rb
+++ app/helpers/coders_helper.rb
@@ -1,2 +1,7 @@
 module CodersHelper
+  def avatar coder, size: '45'
+    img = image_tag coder.avatar_url,
+      alt: coder.github_name, size: size
+    link_to img, coder, class: 'avatar'
+  end
 end
