33c3041d2eabf8d0b7e28dc754a6f11769ca1a2a
lorin <werthenlorin@gmail.com>
2016-06-17 01:28:32 +0200

    Update coders_helper.rb

--- app/helpers/coders_helper.rb
+++ app/helpers/coders_helper.rb
@@ -1,7 +1,7 @@
 module CodersHelper
   def avatar(coder, size: '45', name: false)
     link_to coder, class: 'avatar' do
-      concat image_tag(coder.avatar_url, alt: coder.github_name, size: size)
+      concat image_tag coder.avatar_url, alt: coder.github_name, size: size
       concat coder.github_name if name
     end
   end
