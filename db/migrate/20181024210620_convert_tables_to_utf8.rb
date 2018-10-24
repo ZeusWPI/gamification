1aaac909ff6fdb516d02796dacd3c4db0fae4968
Wout Schellaert <wout.schellaert@gmail.com>
2018-10-24 23:33:44 +0200

    New errors are good

--- db/migrate/20181024210620_convert_tables_to_utf8.rb
+++ db/migrate/20181024210620_convert_tables_to_utf8.rb
@@ -1,19 +1,16 @@
 class ConvertTablesToUtf8 < ActiveRecord::Migration
-  def change_encoding(encoding,collation)
-    connection = ActiveRecord::Base.connection
-    tables = connection.tables
-    dbname = connection.current_database
-    execute <<-SQL
-      ALTER DATABASE #{dbname} CHARACTER SET #{encoding} COLLATE #{collation};
-    SQL
-    tables.each do |tablename|
-      execute <<-SQL
-        ALTER TABLE #{dbname}.#{tablename} CONVERT TO CHARACTER SET #{encoding} COLLATE #{collation};
-      SQL
-    end
-  end
-
   def change
-    change_encoding('utf8mb4','utf8mb4_bin')
+    config = Rails.configuration.database_configuration
+    db_name = config[Rails.env]["database"]
+    collate = 'utf8mb4_polish_ci'
+    char_set = 'utf8mb4'
+    row_format = 'DYNAMIC'
+ 
+    execute("ALTER DATABASE #{db_name} CHARACTER SET #{char_set} COLLATE #{collate};")
+ 
+    ActiveRecord::Base.connection.tables.each do |table|
+      execute("ALTER TABLE #{table} ROW_FORMAT=#{row_format};")
+      execute("ALTER TABLE #{table} CONVERT TO CHARACTER SET #{char_set} COLLATE #{collate};")
+    end
   end
 end
